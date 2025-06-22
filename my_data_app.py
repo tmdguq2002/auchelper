0dc7759e4ffa4a3942744dfc8547146853eac0c3
KwonSeungHyeop <suhy2002@naver.com>
2025-06-22 22:51:57 +0900

    gpt

--- my_data_app.py
+++ my_data_app.py
@@ -50,7 +50,7 @@ with col2:
 st.sidebar.title("기능 선택")
 menu = st.sidebar.radio(
     "원하는 기능을 선택하세요",
-    ("📂 로바스 시각화(준비중)", "🖼️ 이미지 용량 줄이기", "🤖 안도미AI", "👥 준비중")
+    ("📂 로바스 시각화(준비중)", "🖼️ 이미지 용량 줄이기", "🧠 안도미AI", "👥 준비중")
 )
 
 my_df = df
@@ -273,7 +273,7 @@ if menu == "🖼️ 이미지 용량 줄이기":
                     mime="application/zip"
                 )
 
-if menu == "🤖 안도미AI": 
+if menu == "🧠 안도미AI": 
     st.title("📄 LangChain 기반 규정 GPT")
 
     # OpenAI 키 설정
